054024fc407bf1f884426e9717b46a595b4219b5
Grzegorz Belter <gbelter@wp.pl>
2024-10-17 21:11:26 +0200

    game

--- src/components/table/UserTableTop.vue
+++ src/components/table/UserTableTop.vue
@@ -4,10 +4,6 @@ import {useRouter} from "vue-router";
 const router = useRouter();
 
 const userName = ref('');
-
-const goToUserAdd = () => {
-  return router.push({name: 'UserAdd'});
-}
 </script>
 
 <template>
@@ -26,7 +22,6 @@ const goToUserAdd = () => {
       </span>
     </label>
     <RouterLink :to="{ name: 'UserAdd'}"
-                @click="goToUserAdd"
                 class="button">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16">
         <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/>
